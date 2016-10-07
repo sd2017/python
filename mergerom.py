c454cfe35fd86a513088981732da12514b9d85e5
sd2017 <sd2017@walla.co.il>
2016-10-07 15:54:56 +0300

    merge, pool

---
+++ mergerom.py
@@ -0,0 +1,10 @@
+class mergerom():
+    "merge large m small n files"
+    prefix="mergerom."
+    postfix = ".txt"
+    dirname="mergerom"
+    output= "mergerom.all"
+    def  __init__(self):
+      pass
+    def get_file_list(self):
+      pass
