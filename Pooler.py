8350f31719a3bed27b1185059644aa43a4a98fa1
sd2017 <sd2017@walla.co.il>
2016-10-08 18:30:25 +0300

    pooler run,join

--- Pooler.py
+++ Pooler.py
@@ -1,20 +1,22 @@
 import threading
 import time
-
+import Queue
 
 
 class Pooler:
-    def __init__(self, size):
+    def __init__(self, size,running_jobs_counter):
             print "start pooler constructor"
-            self.running_jobs_counter=0
+            self.running_jobs_counter=running_jobs_counter
+            print "running_jobs_counter={0}".format(self.running_jobs_counter)
             self.list=[]
-            self.jobs = []
-            self.condition = threading.Condition()
+            self.jobs = Queue.Queue()
+            self.mutex = threading.Lock()
+            self.condition = threading.Condition(self.mutex)
             print "pooler constructor try acquire "
             self.condition.acquire()
             print "pooler constructor  acquired "
             for  index in range(0,size):
-                threader=Pooler.Threader(self.condition,self.jobs,self.running_jobs_counter)
+                threader=Pooler.Threader(index,self.condition,self.jobs,self.running_jobs_counter)
                 self.list.append(threader)
             print "pooler constructor  created threads  "
             for threader in self.list:
@@ -29,39 +31,43 @@ class Pooler:
         #print "entering global run"
         self.condition.acquire()
         #print " global run acuire lock"
-        self.jobs.append(runnabe)
+        self.jobs.put(runnabe)
         self.condition.notify()
         self.condition.release()
+
+
 
     def join(self):
         print "start pooler join "
         time.sleep(1.0)
-        while ( self.jobs):
-            pass
-        if (not self.jobs):
-            print "iterating join "
-            while (self.running_jobs_counter>0):
-              print "join waiting {0}".format(self.running_jobs_counter)
-            for threader in self.list:
-                 threader.join()
+        self.jobs.join()
+        for threader in self.list:
+            time.sleep(0.5)
+            threader.join()
 
 
     class Threader(threading.Thread):
-        def __init__(self,condition,jobs,running_jobs_counter):
+        def __init__(self,name,condition,jobs,running_jobs_counter):
             super(Pooler.Threader,self).__init__()
             self.running_jobs_counter=running_jobs_counter
             self.condition=condition
             self.jobs=jobs
             self.runnanle=None
             self.running=True
+            self.name=name
+
             print "Threader before start"
 
         def run(self):
            while (self.running):
+                self.runnanle = self.jobs.get(1)  #TODO blocking time
                 self.condition.acquire()
-                if (self.jobs):
-                   self.runnanle = self.jobs.pop()
-                   self.running_jobs_counter=self.running_jobs_counter+1
+                #print("run acquired {}".format(self.name))
+
+                #if (not self.jobs.empty()):
+                #   self.runnanle = self.jobs.get()
+                   #self.running_jobs_counter = self.running_jobs_counter + 1
+                #self.running_jobs_counter = self.running_jobs_counter + 1
                 self.condition.notify()
                 self.condition.release()
 
@@ -69,12 +75,16 @@ class Pooler:
                       #print "poped {0} {1}".format(self.runnanle.name, time.ctime())
                       self.runnanle.entry_time=time.ctime()
                       self.runnanle.run()
+                      self.jobs.task_done()
                       self.condition.acquire()
-                      self.running_jobs_counter = self.running_jobs_counter -1
+                      self.running_jobs_counter[0] = self.running_jobs_counter[0] -1 # TODO rename running_jobs_counter to other name
+                      #print "thread inside finished {0}:{1}".format(self.name,self.running_jobs_counter)
                       self.condition.notify()
                       self.condition.release()
-                      if ((not self.jobs) and self.running_jobs_counter ==0):
-                          self.running=False
-                      self.runnanle=None
+                      self.runnanle = None
+                if ( self.jobs.empty() and  self.running_jobs_counter[0]<5):
+                    print "stop {0}:{1}".format(self.name,self.running_jobs_counter )
+                    self.running=False
+
 
 
--- PoolerTest.py
+++ PoolerTest.py
@@ -14,18 +14,20 @@ class Runnable():
 class PoolerTest(unittest.TestCase):
         def setUp(self):
             print "start test setup"
-            self.pooler=Pooler.Pooler(5)
+            self.pooler=None
             print "pooler initialized"
         def testFive(self):
             print "start testFive"
-            times=[2.00,3.00,9.00,5.00,1.10,9.00,5.00,6.00,8.00,1.00,1.20,10.0,1.30,1.20,4.00,1.00,1.00,1.00,1.00,1.00]
+            times=[1.00,2.00,8.00,4.00,1.10,8.00,4.00,5.00,7.00,1.00,1.20,9.00,1.30,1.20,3.00,1.00,1.00,1.00,1.00,1.00]
             names=["11","12","13","14","15","21","22","23","24","25","31","32","33","34","35","41","42","43","44","45"]
+            self.pooler = Pooler.Pooler(5, [len(times)])
+
             runnables=[];
             for  time,name in zip(times,names):
                 runnables.append(Runnable(time,name))
             for runnable in runnables:
                 self.pooler.run(runnable)
-
+            print "main before join"
             self.pooler.join()
             self.assertEqual(2,1)
 
