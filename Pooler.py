613519d8228b9fdeaac819c9b65c9bc69c5d84b0
sd2017 <sd2017@walla.co.il>
2016-10-08 15:51:48 +0300

    cleanup

--- Pooler.py
+++ Pooler.py
@@ -1,30 +1,80 @@
 import threading
-
-
-class RunnableInterface:
-    def run(self):
-        pass
-
-
-class Threader(threading.Thread):
-    def __init__(self):
-        super(Threader,self).__init__(self.global_run)
-        self.runnanle = RunnableInterface()
-        self.run()
-    def lock(self):
-        pass
-
-    def global_run(self):
-       while (True):
-        self.lock()
-        if (self.runnanle != None):
-           self.runnanle.run()
-           self.runnanle=None
+import time
 
 
 
 class Pooler:
     def __init__(self, size):
-        self.free = []
-        self.list=[]>?>?>?
-            threader=Threader()
+            print "start pooler constructor"
+            self.running_jobs_counter=0
+            self.list=[]
+            self.jobs = []
+            self.condition = threading.Condition()
+            print "pooler constructor try acquire "
+            self.condition.acquire()
+            print "pooler constructor  acquired "
+            for  index in range(0,size):
+                threader=Pooler.Threader(self.condition,self.jobs,self.running_jobs_counter)
+                self.list.append(threader)
+            print "pooler constructor  created threads  "
+            for threader in self.list:
+                threader.start()
+            print "pooler constructor  started threads  "
+            self.condition.notify()
+            print "pooler constructor  notified  "
+            self.condition.release()
+            print "finished pooler constructor"
+
+    def run(self,runnabe):
+        #print "entering global run"
+        self.condition.acquire()
+        #print " global run acuire lock"
+        self.jobs.append(runnabe)
+        self.condition.notify()
+        self.condition.release()
+
+    def join(self):
+        print "start pooler join "
+        time.sleep(1.0)
+        while ( self.jobs):
+            pass
+        if (not self.jobs):
+            print "iterating join "
+            while (self.running_jobs_counter>0):
+              print "join waiting {0}".format(self.running_jobs_counter)
+            for threader in self.list:
+                 threader.join()
+
+
+    class Threader(threading.Thread):
+        def __init__(self,condition,jobs,running_jobs_counter):
+            super(Pooler.Threader,self).__init__()
+            self.running_jobs_counter=running_jobs_counter
+            self.condition=condition
+            self.jobs=jobs
+            self.runnanle=None
+            self.running=True
+            print "Threader before start"
+
+        def run(self):
+           while (self.running):
+                self.condition.acquire()
+                if (self.jobs):
+                   self.runnanle = self.jobs.pop()
+                   self.running_jobs_counter=self.running_jobs_counter+1
+                self.condition.notify()
+                self.condition.release()
+
+                if (self.runnanle != None):
+                      #print "poped {0} {1}".format(self.runnanle.name, time.ctime())
+                      self.runnanle.entry_time=time.ctime()
+                      self.runnanle.run()
+                      self.condition.acquire()
+                      self.running_jobs_counter = self.running_jobs_counter -1
+                      self.condition.notify()
+                      self.condition.release()
+                      if ((not self.jobs) and self.running_jobs_counter ==0):
+                          self.running=False
+                      self.runnanle=None
+
+
--- PoolerTest.py
+++ PoolerTest.py
@@ -1,23 +1,33 @@
 import unittest
 import time
 import Pooler
-class Runnable(Pooler.RunnableInterface):
+class Runnable():
     def __init__(self,num,name):
         self.delay=num
         self.name=name
+        self.entry_time=0
     def run(self):
         time.sleep(self.delay)
-        print "finished {0} {1} ".format( self.name, time.ctime())
+        print "finished {0}:{1} {2} -  {3} ".format( self.name,self.delay,self.entry_time, time.ctime())
 
 
 class PoolerTest(unittest.TestCase):
         def setUp(self):
-            self.pooler=Pooler.Pooler(30)
+            print "start test setup"
+            self.pooler=Pooler.Pooler(5)
+            print "pooler initialized"
         def testFive(self):
-            times=[1,2,3,4,5,1,2,3,4,5]
-            names=["11","12","13","14","15","21","22","23","24","25"]
+            print "start testFive"
+            times=[2.00,3.00,9.00,5.00,1.10,9.00,5.00,6.00,8.00,1.00,1.20,10.0,1.30,1.20,4.00,1.00,1.00,1.00,1.00,1.00]
+            names=["11","12","13","14","15","21","22","23","24","25","31","32","33","34","35","41","42","43","44","45"]
             runnables=[];
-            for  time,name in zip(names,times):
+            for  time,name in zip(times,names):
                 runnables.append(Runnable(time,name))
-            for runnable in runnables
+            for runnable in runnables:
+                self.pooler.run(runnable)
 
+            self.pooler.join()
+            self.assertEqual(2,1)
+
+if __name__=="__main__":
+    unittest.main()
